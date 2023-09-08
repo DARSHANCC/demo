ecebe19b546ddd6defb038ba662fed79442df9d4
DARSHANCC <darshanpatel462002@gmail@gamil.com>
2023-09-08 13:18:52 +0530

    main

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.app')
+@extends('admin.layout.app')
 @section('heading')
     {{ 'index' }}
 @endsection
@@ -7,9 +7,9 @@
 @endsection
 
 @section('bread-crumb')
-    <li class="breadcrumb-item text-muted">Pages</li>
-    <li class="breadcrumb-item text-muted">Account</li>
-    <li class="breadcrumb-item text-dark">Billing</li>
+    <li class="breadcrumb-item text-muted">Index</li>
+    {{-- <li class="breadcrumb-item text-muted">Account</li> --}}
+    {{-- <li class="breadcrumb-item text-dark">Billing</li> --}}
 @endsection
 
 @section('username', 'hello')
